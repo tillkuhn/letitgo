9fd5cb7d6f2ac26ea20bf1e3aa05e3e4f2b1890f
Till Kuhn <till.kuhn@dbschenker.com>
2022-08-16 17:12:03 +0200

    tick tock

---
+++ cmd/ticker.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"fmt"
+	"tillkuhn/goplay/ticker"
+
+	"github.com/spf13/cobra"
+)
+
+// tickerCmd represents the ticker command
+var tickerCmd = &cobra.Command{
+	Use:   "ticker",
+	Short: "Run Task periodically",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("ticker command called")
+		ticker.RunTickerWithChannel()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(tickerCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// tickerCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// tickerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- ticker/ticker.go
+++ ticker/ticker.go
@@ -8,37 +8,54 @@ import (
 // RunTickerWithChannel ticker example based on https://gobyexample.com/tickers
 // Tickers use a similar mechanism to timers: a channel that is sent values.
 // Here we’ll use the select builtin on the channel to await the values as they arrive every 500ms.
+// https://stackoverflow.com/a/31936175/4292075
 func RunTickerWithChannel() {
+	var tickCount int
+	tickerFunc := func(t time.Time) {
+		tickCount++
+		fmt.Println("Tick tock at", t, "tickCount=", tickCount)
+	}
+	doneChan := RunTickerFuncWithChannel(tickerFunc, 500*time.Millisecond)
+	time.Sleep(1600 * time.Millisecond)
+	doneChan <- true
+	fmt.Printf("Function has been executed %d times\n", tickCount)
+	time.Sleep(1600 * time.Millisecond)
+}
 
-	ticker := time.NewTicker(500 * time.Millisecond)
-	done := make(chan bool)
+// RunTickerFuncWithChannel runs the ticket function once immediately, afterwards in regular intervals
+// until
+func RunTickerFuncWithChannel(tickerFunc func(t time.Time), interval time.Duration) chan bool {
+
+	doneChan := make(chan bool)
 	go func() {
+		ticker := time.NewTicker(interval)
+		defer ticker.Stop()
+
+		tickerFunc(time.Now())
 		for {
 			select {
-			case <-done:
+			case <-doneChan:
+				fmt.Println("We're done here")
 				return
 			case t := <-ticker.C:
-				fmt.Println("Tick at", t)
+				tickerFunc(t)
 			}
 		}
 	}()
+	return doneChan
 
-	time.Sleep(1600 * time.Millisecond)
-	ticker.Stop()
-	done <- true
-	fmt.Println("Ticker stopped")
 }
 
 // RunTickerSimple simplified version based on https://stackoverflow.com/a/53057336/4292075
-func RunTickerSimple() {
-	ticker := time.NewTicker(1 * time.Second)
-	go func() {
-		for range ticker.C {
-			fmt.Println("Hello !!")
-		}
-	}()
-
-	// wait for 10 seconds
-	time.Sleep(10 * time.Second)
-	ticker.Stop()
-}
+//func RunTickerSimple() {
+//	ticker := time.NewTicker(1 * time.Second)
+//	go func() {
+//		for range ticker.C {
+//			fmt.Println("Hello !!")
+//		}
+//	}()
+//
+//	// wait for 10 seconds
+//	time.Sleep(10 * time.Second)
+//	ticker.Stop()
+//}
